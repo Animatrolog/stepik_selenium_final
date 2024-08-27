5ca3ec4fa8247b4797683eac6bd8b5b0aea9d871
Ivan <darkheacrab@gmail.com>
2024-08-27 22:28:13 +0500

    add basket page tests

--- pages/locators.py
+++ pages/locators.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.by import By
 class BasePageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+    BASKET_LINK = (By.CSS_SELECTOR, ".basket-mini span>a")
 
 
 class LoginPageLocators:
@@ -18,3 +19,8 @@ class ProductPageLocators:
     BASKET_PRICE = (By.CSS_SELECTOR, "div.alertinner>p>strong")
     BASKET_PRODUCT_NAME = (By.CSS_SELECTOR, "#messages>div:nth-child(1)>div>strong")
     SUCCESS_MESSAGE = (By.CSS_SELECTOR, "#messages>div:nth-child(1)")
+
+
+class BasketPageLocators:
+    BASKET_EMPTY_MESSAGE = (By.CSS_SELECTOR, "div#content_inner>p")
+    BASKET_ITEMS = (By.CSS_SELECTOR, ".basket-items")
